5d60953319963d1cae060b3a04ef603f1aa31110
unimity <admin@unimity.com>
2017-07-11 15:37:42 +0530

    ipn paypal2

--- docroot/modules/custom/paypal/src/Form/thankyoupage.php
+++ docroot/modules/custom/paypal/src/Form/thankyoupage.php
@@ -36,70 +36,47 @@ class thankyoupage extends FormBase {
  * {@inheritdoc}
  */
 public function buildForm(array $form, FormStateInterface $form_state) {
- 	 // pdt tokens
-    			 
+ 	 // pdt tokens   			 
  if(!empty($_GET['tx'])){
-   $pp_hostname = "www.sandbox.paypal.com"; // Change to www.sandbox.paypal.com to test against sandbox
-// read the post from PayPal system and add 'cmd'
-
-$req = 'cmd=_notify-synch';
- 
-$tx_token = $_GET['tx'];
-$auth_token = "GfqCw6b45-T-AZYVEMwCwy0IB4gNCO8QXVvHd0irgnbk4hhB80Xm437MddW";
-$req .= "&tx=$tx_token&at=$auth_token";
- 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://$pp_hostname/cgi-bin/webscr");
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
-//set cacert.pem verisign certificate path in curl using 'CURLOPT_CAINFO' field here,
-//if your server does not bundled with default verisign certificates.
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array("Host: $pp_hostname"));
-$res = curl_exec($ch);
-curl_close($ch);
-if(!$res){
-    //HTTP ERROR
-}else{
-     // parse the data
-    $lines = explode("\n", trim($res));
-    $keyarray = array();
-    if (strcmp ($lines[0], "SUCCESS") == 0) {
-        for ($i = 1; $i < count($lines); $i++) {
-            $temp = explode("=", $lines[$i],2);
-            $keyarray[urldecode($temp[0])] = urldecode($temp[1]);
-        }
-    // check the payment_status is Completed
-    // check that txn_id has not been previously processed
-    // check that receiver_email is your Primary PayPal email
-    // check that payment_amount/payment_currency are correct
-    // process payment
-    $firstname = $keyarray['first_name'];
-    $lastname = $keyarray['last_name'];
-    $itemname = $keyarray['item_name'];
-    $amount = $keyarray['payment_gross'];
-     
-    echo ("<p><h3>Thank you for your purchase!</h3></p>");
-     
-    echo ("<b>Payment Details</b><br>\n");
-    echo ("<li>Name: $firstname $lastname</li>\n");
-    echo ("<li>Item: $itemname</li>\n");
-    echo ("<li>Amount: $amount</li>\n");
-   dpm($lines[0]);
+   
+    function pdt_token($tx){
+      $pp_hostname = "www.sandbox.paypal.com"; // Change to www.sandbox.paypal.com to test against sandbox
+      // read the post from PayPal system and add 'cmd'
+      $req = 'cmd=_notify-synch';
+      $tx_token = $tx;
+      $auth_token = "GfqCw6b45-T-AZYVEMwCwy0IB4gNCO8QXVvHd0irgnbk4hhB80Xm437MddW";
+      $req .= "&tx=$tx_token&at=$auth_token";
+      $ch = curl_init();
+       curl_setopt($ch, CURLOPT_URL, "https://$pp_hostname/cgi-bin/webscr");
+       curl_setopt($ch, CURLOPT_POST, 1);
+       curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+       curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+       //set cacert.pem verisign certificate path in curl using 'CURLOPT_CAINFO' field here,
+       //if your server does not bundled with default verisign certificates.
+       curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+       curl_setopt($ch, CURLOPT_HTTPHEADER, array("Host: $pp_hostname"));
+       $res = curl_exec($ch);
+       curl_close($ch);
+       if (!$res){
+	   //HTTP ERROR
+       } else {
+	    // parse the data
+	   $lines = explode("\n", trim($res));
+	   $keyarray = array();
+	   if (strcmp ($lines[0], "SUCCESS") == 0) {
+	      $status = $lines[0];
+	   }
+	   else if (strcmp ($lines[0], "FAIL") == 0) {
+	      $status = $lines[0];
+	   }
+	}
+	return $status;
     }
-    else if (strcmp ($lines[0], "FAIL") == 0) {
-        dpm($lines[0]);
-	// log for manual investigation
-    }
-     }
-     
-     
-     
-     
-     }
-
+     $tx = $_GET['tx'];
+    $a = pdt_token($tx);
+    dpm($a);
+  }
           // str_replace($a,'Lorem',$a);
 
 			
