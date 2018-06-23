c2dbe4772e6b306b9f3acc6f9b2c6cc90061e68d
Krithika <krithika@unimity.com>
2018-06-23 15:22:37 +0530

    zip

--- docroot/modules/custom/custom_zip_download/src/Plugin/Action/ZipDownload.php
+++ docroot/modules/custom/custom_zip_download/src/Plugin/Action/ZipDownload.php
@@ -59,7 +59,8 @@ class ZipDownload extends ActionBase {
       //   $zip->addFile($src, $user_name[0]['value'].'/'.$filename);
       //   $options = array('add_path' => $user_name[0]['value'].'/', 'remove_all_path' => TRUE);
       // $zip->addGlob('../private/student_documents/general_documents/'.$get_users[0]['value'].'/'.'*.{txt,doc,pdf,docx}', GLOB_BRACE, $options);
-      print_r($get_users[0]['value']);
+      $profile = Profile::load($get_users[0]['value']);
+      print_r($profile);
         //file_prepare_directory($dest_1, FILE_CREATE_DIRECTORY);
         //$path = str_replace('private:/',$base_path,$dest_1);
         //copy($src, $path.'/'.$filename);
