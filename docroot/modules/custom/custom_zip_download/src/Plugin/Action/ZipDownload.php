de34a40cafc64b06748140369d5c47b9452c2f3a
Krithika <krithika@unimity.com>
2018-07-13 18:27:30 +0530

    import description

--- docroot/modules/custom/custom_zip_download/src/Plugin/Action/ZipDownload.php
+++ docroot/modules/custom/custom_zip_download/src/Plugin/Action/ZipDownload.php
@@ -74,7 +74,7 @@ class ZipDownload extends ActionBase {
         $dest_1 = $new_folder.'/'.$user_name[0]['value'];
         $path = str_replace('private:/',$base_path,$dest_1);
         //$dest_1 = str_replace('private:/',$base_path,$dest_1);
-        $zip->addEmptyDir($user_name[0]['value']);
+        $zip->addEmptyDir($get_users[0]['value'].'-'.$user_name[0]['value']);
         $zip->addFile($src, $user_name[0]['value'].'/'.$filename);
         $options = array('add_path' => $user_name[0]['value'].'/', 'remove_all_path' => TRUE);
       $zip->addGlob('../private/student_documents/general_documents/'.$get_users[0]['value'].'/'.'*.{txt,doc,pdf,docx}', GLOB_BRACE, $options);
